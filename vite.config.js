07ed6c6ed4a1c4a61d64a931536773476e11d868
Hussain Aminu <hussain_aminu@yahoo.com>
2022-04-02 11:35:40 +0100

    little reshape

--- vite.config.js
+++ vite.config.js
@@ -3,3 +3,17 @@ export default {
   // config options
   //   base: "/publicify/",
 };
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        main: resolve(__dirname, "about.html"),
+        main: resolve(__dirname, "contact.html"),
+      },
+    },
+  },
+});
